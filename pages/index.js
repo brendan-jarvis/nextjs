ecf16939c2fe97afcb9128f47d3a6a477bae5384
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-01-10 14:45:38 +1300

    Use getStaticProps to get list of posts from Supabase

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import { supabase } from '../lib/supabaseClient'
 import Link from 'next/link'
-import Date from '../components/date'
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  let { data: posts, error } = await supabase
+    .from('posts')
+    .select('*')
+    .order('created_at', { ascending: false })
+
   return {
     props: {
-      allPostsData,
+      allPostsData: posts,
     },
   }
 }
@@ -26,6 +29,10 @@ export default function Home({ allPostsData }) {
           Next.js!
         </p>
         <p>
+          The front-end is hosted on Vercel and the backend is provided by
+          Supabase.
+        </p>
+        <p>
           You can contact me on{' '}
           <a href="https://twitter.com/brendanjjarvis">Twitter</a>.
         </p>
@@ -34,12 +41,16 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, created_at, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                {new Intl.DateTimeFormat('en-NZ', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                }).format(new Date(created_at))}
               </small>
             </li>
           ))}
