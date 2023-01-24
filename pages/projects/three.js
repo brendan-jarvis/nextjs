74833469fd75a1a00f3f886755fb97e59d7c14fd
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-01-24 15:46:14 +1300

    Rainbow Three.js tutorial

--- pages/projects/three.js
+++ pages/projects/three.js
@@ -1,6 +1,29 @@
 import Layout from '../../components/layout'
 import Head from 'next/head'
-import utilStyles from '../../styles/utils.module.css'
+import { Stars } from '@react-three/drei'
+
+import { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Rainbow } from './Rainbow'
+
+function Scene() {
+  const ref = useRef()
+  useFrame((state, delta) => (ref.current.rotation.z += delta / 5))
+  return (
+    <>
+      <Rainbow ref={ref} startRadius={0} endRadius={0.65} fade={0} />
+      <Stars
+        radius={100}
+        depth={50}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      />
+    </>
+  )
+}
 
 export default function Three({ session }) {
   return (
@@ -8,10 +31,10 @@ export default function Three({ session }) {
       <Head>
         <title>Three.js</title>
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>Three.js</h1>
-        <p>Coming soon...</p>
-      </article>
+      <Canvas>
+        <color attach="background" args={['black']} />
+        <Scene />
+      </Canvas>
     </Layout>
   )
 }
