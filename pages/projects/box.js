540d898863a368ee0aff67a0506ccbc190929fbb
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-01-26 11:49:14 +1300

    Add camera and on-screen controls

--- pages/projects/box.js
+++ pages/projects/box.js
@@ -1,20 +1,20 @@
 import React, { useRef } from 'react'
 import Head from 'next/head'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Stars } from '@react-three/drei'
+import { CameraControls, Stars } from '@react-three/drei'
 
 const Scene = () => {
   const myMesh = useRef()
+
   // Rotate x, y, and z axis
   useFrame(() => {
     myMesh.current.rotation.x += 0.01
-    myMesh.current.rotation.y += 0.03
-    myMesh.current.rotation.z += 0.02
+    myMesh.current.rotation.y += 0.02
+    myMesh.current.rotation.z += 0.03
   })
 
   return (
     <>
-      <pointLight position={[0, 10, 10]} intensity={1.5} />
       <ambientLight intensity={0.1} />
       <mesh ref={myMesh}>
         <dodecahedronGeometry attach="geometry" args={[1, 0]} />
@@ -35,15 +35,69 @@ const Scene = () => {
 }
 
 const Box = () => {
+  const cameraControlRef = useRef()
+
   return (
     <>
       <Head>
         <title>Three.js</title>
       </Head>
       <Canvas>
+        <CameraControls ref={cameraControlRef} />
         <color attach="background" args={['black']} />
         <Scene />
       </Canvas>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: '0',
+          margin: 'auto',
+          border: '1px solid cornflowerblue',
+          padding: '10px',
+        }}
+      >
+        <p>Camera controls</p>
+        <button
+          type="button"
+          onClick={() => {
+            cameraControlRef.current?.rotate(0, -Math.PI / 4, true)
+          }}
+        >
+          Up
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            cameraControlRef.current?.rotate(0, Math.PI / 4, true)
+          }}
+        >
+          Down
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            cameraControlRef.current?.rotate(-Math.PI / 4, 0, true)
+          }}
+        >
+          Left
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            cameraControlRef.current?.rotate(Math.PI / 4, 0, true)
+          }}
+        >
+          Right
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            cameraControlRef.current?.reset(true)
+          }}
+        >
+          Reset
+        </button>
+      </div>
     </>
   )
 }
