a5dc7c3c7684acbc5dd6dd0a18c2773b763d22b4
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-01-27 15:34:04 +1300

    Add keyboard controls

--- pages/projects/asteroids.js
+++ pages/projects/asteroids.js
@@ -1,15 +1,45 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import Head from 'next/head'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { CameraControls, Stars } from '@react-three/drei'
+import {
+  CameraControls,
+  KeyboardControls,
+  KeyboardControlsEntry,
+  Stars,
+} from '@react-three/drei'
 
 const Spaceship = () => {
   const meshRef = useRef()
+  const speed = 0.1
 
-  useFrame(() => {
-    meshRef.current.rotation.x += 0.01
-    meshRef.current.rotation.y += 0.01
-  })
+  // useFrame(() => {
+  //   meshRef.current.rotation.x += 0.01
+  //   meshRef.current.rotation.y += 0.01
+  // })
+
+  useEffect(() => {
+    const onKeyDown = (e) => {
+      switch (e.key) {
+        case 'ArrowUp':
+          meshRef.current.position.y += speed
+          break
+        case 'ArrowDown':
+          meshRef.current.position.y -= speed
+          break
+        case 'ArrowLeft':
+          meshRef.current.rotation.z += speed
+          break
+        case 'ArrowRight':
+          meshRef.current.rotation.z -= speed
+          break
+        default:
+          break
+      }
+    }
+
+    window.addEventListener('keydown', onKeyDown)
+    return () => window.removeEventListener('keydown', onKeyDown)
+  }, [speed])
 
   return (
     <mesh ref={meshRef} position={[0, 0, 0]}>
