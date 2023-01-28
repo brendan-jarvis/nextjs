dfbe0c2f7b56971e9cb17b8e4eca37779c728b8f
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-01-28 16:50:02 +1300

    Remove Scene and add castshadow to Spaceship

--- pages/projects/asteroids.js
+++ pages/projects/asteroids.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react'
 import Head from 'next/head'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { CameraControls, Stars } from '@react-three/drei'
 
 const Spaceship = () => {
@@ -32,35 +32,10 @@ const Spaceship = () => {
   }, [speed])
 
   return (
-    <mesh ref={meshRef} position={[0, 0, 0]}>
+    <mesh castShadow ref={meshRef} position={[0, 0, 0]}>
       <tetrahedronGeometry />
       <meshStandardMaterial color={'white'} />
     </mesh>
-  )
-}
-
-const Scene = () => {
-  return (
-    <>
-      <ambientLight intensity={0.1} />
-
-      <directionalLight
-        position={[1, 1, 1]}
-        intensity={2}
-        castShadow
-        color={'#F4E99B'}
-      />
-
-      <Stars
-        radius={100}
-        depth={50}
-        count={5000}
-        factor={4}
-        saturation={0}
-        fade
-        speed={1}
-      />
-    </>
   )
 }
 
@@ -72,14 +47,30 @@ const Asteroids = () => {
       <Head>
         <title>Asteroids</title>
       </Head>
-      <Canvas
-        camera={{ fov: 90, near: 0.1, far: 1000, position: [0, 0, 10] }}
-        shadows
-      >
+      <Canvas camera={{ fov: 90, position: [0, 0, 10] }} shadows>
         <CameraControls ref={cameraControlRef} />
         <color attach="background" args={['black']} />
         <Spaceship />
-        <Scene />
+        <ambientLight intensity={0.1} />
+
+        <spotLight
+          angle={0.25}
+          penumbra={0.5}
+          position={[10, 10, 5]}
+          intensity={2}
+          color={'#F4E99B'}
+          castShadow
+        />
+
+        <Stars
+          radius={100}
+          depth={50}
+          count={5000}
+          factor={4}
+          saturation={0}
+          fade
+          speed={1}
+        />
       </Canvas>
     </>
   )
