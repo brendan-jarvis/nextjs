ed8576f41d3a3fc21f0942fbaa2138f411f29ab7
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-01-31 16:18:38 +1300

    Trying to implement AsteroidSpawner

--- pages/projects/asteroids.js
+++ pages/projects/asteroids.js
@@ -1,12 +1,48 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import Head from 'next/head'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { CameraControls, Stars } from '@react-three/drei'
+
+const AsteroidSpawner = ({ count }) => {
+  const asteroidRef = useRef()
+  const [asteroids, setAsteroids] = useState([])
+  const speed = 0.1
+
+  const bearingX = Math.random() * 2 - 1
+  const bearingY = Math.random() * 2 - 1
+
+  useFrame((state, delta) => {
+    asteroidRef.current.position.x += bearingX * speed
+    asteroidRef.current.position.y += bearingY * speed
+  })
+
+  for (let i = 0; i < count; i++) {
+    asteroids.push(
+      <mesh ref={asteroidRef} key={i} castShadow receiveShadow>
+        <octahedronGeometry
+          attach="geometry"
+          args={[1, 0]}
+          radius={Math.random()}
+          detail={Math.floor(Math.random() * 3) + 1}
+          position={[Math.random() * 100, Math.random() * 100, 0]}
+        />
+        <meshStandardMaterial
+          attach="material"
+          color={`hsl(${Math.random() * 360}, 100%, 50%)`}
+          roughness={0.5}
+        />
+      </mesh>
+    )
+  }
+
+  return <>{asteroids}</>
+}
 
 const Spaceship = () => {
   const spaceshipRef = useRef()
   const accelerationRef = useRef(0.0)
   const speed = 0.1
+  const turnSpeed = 0.3
 
   useFrame((state, delta) => {
     spaceshipRef.current.position.y += accelerationRef.current
@@ -22,10 +58,10 @@ const Spaceship = () => {
           accelerationRef.current -= speed
           break
         case 'ArrowLeft':
-          spaceshipRef.current.rotation.z += speed
+          spaceshipRef.current.rotation.z += turnSpeed
           break
         case 'ArrowRight':
-          spaceshipRef.current.rotation.z -= speed
+          spaceshipRef.current.rotation.z -= turnSpeed
           break
         default:
           break
@@ -37,15 +73,9 @@ const Spaceship = () => {
   }, [speed])
 
   return (
-    // <mesh ref={spaceshipRef} castShadow receiveShadow>
-    //   {/* <tetrahedronGeometry /> */}
-    //   <coneGeometry args={[1, 2, 20]} radialSegments="3" />
-    //   <meshStandardMaterial color={'white'} />
-    // </mesh>
-
     <mesh ref={spaceshipRef} castShadow receiveShadow>
       <coneGeometry radius={5} height={20} radialSegments={3} />
-      <meshStandardMaterial color={'white'} roughness={0.5} />
+      <meshStandardMaterial color={'silver'} roughness={0.5} metalness={0.8} />
     </mesh>
   )
 }
@@ -62,12 +92,13 @@ const Asteroids = () => {
         <CameraControls ref={cameraControlRef} />
         <color attach="background" args={['black']} />
         <Spaceship />
-        <ambientLight intensity={0.1} />
+        <AsteroidSpawner count={100} />
+        <ambientLight intensity={0.5} />
 
         <directionalLight
           position={[10, 10, 10]}
           intensity={2}
-          color={'#F4E99B'}
+          color={'#FFF'}
           castShadow
         />
 
