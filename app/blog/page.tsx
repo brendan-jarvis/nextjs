c26090291e65e741a3f3fdaa8f51b98ab8091237
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-10-24 15:17:15 +1300

    List of blog posts

---
+++ app/blog/page.tsx
@@ -0,0 +1,22 @@
+import { createClient } from '@/utils/supabase/server'
+import Link from 'next/link'
+import dayjs from 'dayjs'
+
+export const dynamic = 'force-dynamic'
+
+export default async function Page() {
+  const supabase = createClient()
+  const { data: posts } = await supabase.from('posts').select()
+
+  return (
+    <ul className="my-auto text-foreground">
+      {posts?.map((post) => (
+        <Link key={post.id} href={`blog/${post.id}`}>
+          <li>
+            {post.title} - {dayjs(post.created_at).format('DD MMM YYYY')}
+          </li>
+        </Link>
+      ))}
+    </ul>
+  )
+}
