6f01356d384aec0e9dcc9b2fa066546cb1d88d3d
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-10-26 14:11:01 +1300

    Add back button

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,40 +1,51 @@
-import { createClient } from '@/utils/supabase/server'
-import dayjs from 'dayjs'
-import { remark } from 'remark'
-import remarkHtml from 'remark-html'
+import { createClient } from "@/utils/supabase/server";
+import dayjs from "dayjs";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
+import { Button } from "components/ui/button";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 
 // export const dynamic = 'force-dynamic'
 
 export default async function Page({ params }: { params: { id: number } }) {
-  const supabase = createClient()
+  const supabase = createClient();
   const { data: post } = await supabase
-    .from('posts')
+    .from("posts")
     .select()
-    .eq('id', params.id)
-    .single()
+    .eq("id", params.id)
+    .single();
 
   if (!post) {
     return (
       <div className="mx-auto">
         <h1 className="text-4xl font-bold py-4 text-center">Post not found</h1>
       </div>
-    )
+    );
   }
 
-  const htmlContent = await remark().use(remarkHtml).process(post.content)
+  const htmlContent = await remark().use(remarkHtml).process(post.content);
 
   return (
     <div className="mx-auto">
       <h1 className="text-4xl font-bold py-4 text-center">{post.title}</h1>
       <p className="text-sm text-center py-2">
-        {dayjs(post.created_at).format('DD MMM YYYY')}
+        {dayjs(post.created_at).format("DD MMM YYYY")}
         {post.updated_at !== post.created_at &&
-          ` (updated ${dayjs(post.updated_at).format('DD MMM YYYY')})`}
+          ` (updated ${dayjs(post.updated_at).format("DD MMM YYYY")})`}
       </p>
       <div
         className="prose prose-stone mx-auto"
         dangerouslySetInnerHTML={{ __html: String(htmlContent) }}
       />
+      <div className="flex justify-center">
+        <Button asChild variant="ghost">
+          <Link href="/blog" className="underline font-normal">
+            <ChevronLeft />
+            Back to blog
+          </Link>
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
