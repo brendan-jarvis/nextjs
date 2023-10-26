0e7259268b8c6585bfc8102d8a1e828492642c0e
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-10-26 14:50:32 +1300

    Add colour and styling

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -28,14 +28,16 @@ export default async function Page({ params }: { params: { id: number } }) {
 
   return (
     <div className="mx-auto">
-      <h1 className="text-4xl font-bold py-4 text-center">{post.title}</h1>
-      <p className="text-sm text-center py-2">
+      <h1 className="text-3xl bg-seafoam-green uppercase font-bold py-2 my-2 text-center">
+        {post.title}
+      </h1>
+      <p className="text-sm text-center bg-sunny-yellow my-2 p-1">
         {dayjs(post.created_at).format("DD MMM YYYY")}
         {post.updated_at !== post.created_at &&
           ` (updated ${dayjs(post.updated_at).format("DD MMM YYYY")})`}
       </p>
       <div
-        className="prose prose-stone mx-auto"
+        className="prose prose-stone prose-h2:bg-citrus-blaze prose-h2:text-slate-800 prose-h2:lowercase mx-auto"
         dangerouslySetInnerHTML={{ __html: String(htmlContent) }}
       />
       <div className="flex justify-center">
