ee99130a566cb3c1a3ce10e367c129186677ed64
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-10-27 15:16:53 +1300

    Size blog page to screen

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -27,20 +27,20 @@ export default async function Page({ params }: { params: { id: number } }) {
   const htmlContent = await remark().use(remarkHtml).process(post.content);
 
   return (
-    <div className="mx-auto">
-      <h1 className="bg-seafoam-green my-2 py-2 text-center text-3xl font-bold uppercase">
+    <div className="mx-auto min-h-screen">
+      <h1 className="my-2 bg-seafoam-green py-2 text-center text-3xl font-bold uppercase">
         {post.title}
       </h1>
-      <p className="bg-sunny-yellow my-2 p-1 text-center text-sm">
+      <p className="my-2 bg-sunny-yellow p-1 text-center text-sm">
         {dayjs(post.created_at).format("DD MMM YYYY")}
         {post.updated_at !== post.created_at &&
           ` (updated ${dayjs(post.updated_at).format("DD MMM YYYY")})`}
       </p>
       <div
-        className="prose prose-stone prose-h2:bg-citrus-blaze prose-h2:lowercase prose-h2:text-slate-800 mx-auto"
+        className="prose prose-stone mx-auto prose-h2:bg-citrus-blaze prose-h2:lowercase prose-h2:text-slate-800"
         dangerouslySetInnerHTML={{ __html: String(htmlContent) }}
       />
-      <div className="flex justify-center">
+      <div className="mt-8 flex justify-center">
         <Button asChild variant="ghost">
           <Link href="/blog" className="font-normal underline">
             <ChevronLeft />
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,17 +6,25 @@ export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const supabase = createClient();
-  const { data: posts } = await supabase.from("posts").select();
+  const { data: posts } = await supabase
+    .from("posts")
+    .select()
+    .order("created_at", { ascending: false });
 
   return (
-    <ul className="my-auto text-foreground">
-      {posts?.map((post) => (
-        <Link key={post.id} href={`blog/${post.id}`}>
-          <li className="underline">
-            {post.title} - {dayjs(post.created_at).format("DD MMM YYYY")}
-          </li>
-        </Link>
-      ))}
-    </ul>
+    <div className="min-h-screen items-center">
+      <section className="text-center">
+        <h2 className="my-4 text-6xl font-bold">Blog</h2>
+      </section>
+      <ul className="my-auto leading-loose text-foreground">
+        {posts?.map((post) => (
+          <Link key={post.id} href={`blog/${post.id}`}>
+            <li className="underline">
+              {post.title} - {dayjs(post.created_at).format("DD MMM YYYY")}
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
   );
 }
