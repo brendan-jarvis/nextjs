e260ad54f70e01b2ea4509a983ea30e01b021477
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-10-27 15:15:38 +1300

    Add margin top

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
+    <footer className="mt-8 flex w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
       <p>
         Powered by{" "}
         <Link
