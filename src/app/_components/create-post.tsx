12069a89fe90e75888a7e19f3d398a1c7ffbd3f3
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-11-25 15:36:11 +1300

    Update to match schema

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -7,12 +7,20 @@ import { api } from "~/trpc/react";
 
 export function CreatePost() {
   const router = useRouter();
-  const [name, setName] = useState("");
+  const [post, setPost] = useState({
+    author_id: 1,
+    title: "",
+    content: "",
+  });
 
   const createPost = api.post.create.useMutation({
     onSuccess: () => {
       router.refresh();
-      setName("");
+      setPost({
+        author_id: 1,
+        title: "",
+        content: "",
+      });
     },
   });
 
@@ -20,15 +28,22 @@ export function CreatePost() {
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        createPost.mutate({ name });
+        createPost.mutate(post);
       }}
       className="flex flex-col gap-2"
     >
       <input
         type="text"
         placeholder="Title"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        value={post.title}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+        className="w-full rounded-full px-4 py-2 text-black"
+      />
+      <input
+        type="text"
+        placeholder="Content"
+        value={post.content}
+        onChange={(e) => setPost({ ...post, content: e.target.value })}
         className="w-full rounded-full px-4 py-2 text-black"
       />
       <button
