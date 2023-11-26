1a70964e9bee2e80f0c2a116593108ef5c2627ab
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-11-26 23:46:44 +1300

    More readable

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -21,14 +21,19 @@ export const mysqlTable = mysqlTableCreator((name) => `blog_${name}`);
 export const posts = mysqlTable(
   "post",
   {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+    id: bigint("id", { mode: "number" })
+      .primaryKey()
+      .autoincrement(),
     author_id: bigint("author_id", { mode: "number" }).notNull(),
     title: varchar("name", { length: 256 }),
     content: varchar("content", { length: 4000 }),
     created_at: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updated_at: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`).onUpdateNow().notNull(),
+    updated_at: timestamp("updated_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .onUpdateNow()
+      .notNull(),
     tags: varchar("tags", { length: 256 }),
   },
   (example) => ({
@@ -39,25 +44,34 @@ export const posts = mysqlTable(
 export const comments = mysqlTable(
   "comment",
   {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    post_id: bigint("post_id", { mode: "number" }).notNull(),
+    id: bigint("id", { mode: "number" })
+      .primaryKey()
+      .autoincrement(),
+    post_id: bigint("post_id", { mode: "number" })
+      .notNull(),
     author_id: bigint("author_id", { mode: "number" }).notNull(),
     content: varchar("content", { length: 500 }),
     created_at: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updated_at: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`).onUpdateNow().notNull(),
+    updated_at: timestamp("updated_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .onUpdateNow()
+      .notNull(),
   },
   (example) => ({
-    contentIndex: index("content_idx").on(example.content),
+    contentIndex: index("author_id_idx").on(example.author_id),
   }),
 );
 
 export const profiles = mysqlTable(
   "profile",
   {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    username: varchar("username", { length: 256 }).unique(),
+    id: bigint("id", { mode: "number" })
+      .primaryKey()
+      .autoincrement(),
+    username: varchar("username", { length: 256 })
+      .unique(),
     avatar_url: varchar("avatar_url", { length: 256 }),
   },
   (example) => ({
