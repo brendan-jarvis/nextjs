6c8cfa013c12787b37ccd5255086cded9b29573b
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-12-04 20:53:17 +1300

    Check authorId from context

--- src/server/api/routers/comment.ts
+++ src/server/api/routers/comment.ts
@@ -1,24 +1,35 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  privateProcedure,
+} from "~/server/api/trpc";
 import { comments } from "~/server/db/schema";
 
-import { eq } from 'drizzle-orm';
+import { eq, and } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
 
 export const commentRouter = createTRPCRouter({
   create: publicProcedure
     .input(
       z.object({
         post_id: z.number(),
-        author_id: z.number(),
         content: z.string().min(1).max(1000),
-      })
-
+      }),
     )
     .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      if (!authorId) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+        });
+      }
+
       await ctx.db.insert(comments).values({
         post_id: input.post_id,
-        author_id: input.author_id,
+        author_id: authorId,
         content: input.content,
       });
     }),
@@ -31,18 +42,29 @@ export const commentRouter = createTRPCRouter({
     )
     .query(({ ctx, input }) => {
       return ctx.db.query.comments.findMany({
-        where: eq(comments.post_id, input.post_id)
+        where: eq(comments.post_id, input.post_id),
       });
     }),
 
-  deleteById: publicProcedure
+  deleteById: privateProcedure
     .input(
       z.object({
         id: z.number(),
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.delete(comments).where(eq(comments.id, input.id));
-    }),
+      const authorId = ctx.userId;
 
-}); 
+      if (!authorId) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+        });
+      }
+
+      await ctx.db
+        .delete(comments)
+        .where(
+          and(eq(comments.id, input.id), eq(comments.author_id, authorId)),
+        );
+    }),
+});
