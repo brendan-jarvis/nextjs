656e76657559b6b3012b332452f83395522b0248
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-12-05 23:03:10 +1300

    Pass currentUserId to MoreMenu

--- src/app/_components/Blog/Comments.tsx
+++ src/app/_components/Blog/Comments.tsx
@@ -1,32 +1,43 @@
 import dayjs from "dayjs";
 import { CreateComment } from "./CreateComment";
 import { MoreMenu } from "./MoreMenu";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, currentUser } from "@clerk/nextjs";
 import { clerkClient } from "@clerk/nextjs";
-import { Avatar, AvatarFallback, AvatarImage } from "@/app/_components/ui/avatar"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/app/_components/ui/avatar";
 import { api } from "~/trpc/server";
 import type { SelectComment } from "~/server/db/schema";
 
 async function CommentView({ comment }: { comment: SelectComment }) {
-  const user = await clerkClient.users.getUser(comment.author_id);
+  const commentUser = await clerkClient.users.getUser(comment.author_id);
+  const user = await currentUser();
 
   return (
     <div className="my-4 flex w-full">
-      <Avatar >
-        <AvatarImage src={user.imageUrl} />
+      <Avatar>
+        <AvatarImage src={commentUser.imageUrl} />
         <AvatarFallback>
-          {user.username ? user.username.slice(0, 1) : "??"}
+          {commentUser.username ? commentUser.username.slice(0, 1) : "??"}
         </AvatarFallback>
       </Avatar>
       <div className="ml-2 flex-grow">
         <div className="flex items-center justify-between">
           <p className="text-sm text-gray-600">
-            {user.username ? user.username : user.firstName} -{" "}
-            {dayjs(comment.created_at).format("DD MMM YYYY")}
+            {commentUser.username
+              ? commentUser.username
+              : commentUser.firstName}{" "}
+            - {dayjs(comment.created_at).format("DD MMM YYYY")}
             {comment.updated_at > comment.created_at &&
               ` (updated ${dayjs(comment.updated_at).format("DD MMM YYYY")})`}
           </p>
-          <MoreMenu authorId={comment.author_id} commentId={comment.id} />
+          <MoreMenu
+            currentUserId={user ? user.id : ""}
+            authorId={comment.author_id}
+            commentId={comment.id}
+          />
         </div>
         <p className="text-gray-800">{comment.content}</p>
       </div>
@@ -73,10 +84,7 @@ export default async function Comments({ post_id }: { post_id: number }) {
         <p className="text-sm text-gray-600">Sign in to comment!</p>
       </SignedOut>
       {comments.map((comment) => (
-        <CommentView
-          key={comment.id}
-          comment={comment}
-        />
+        <CommentView key={comment.id} comment={comment} />
       ))}
     </div>
   );
