0c22af38d1996083af9bec41c7a0199be7419ece
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-12-05 13:22:26 +1300

    Update footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -2,17 +2,42 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="mt-8 flex w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
+    <footer className="mt-8 flex w-full flex-col justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
       <p>
         Powered by{" "}
         <Link
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+          href="https://planetscale.com/"
           target="_blank"
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Supabase
+         Planetscale 
+        </Link>{", "}
+        <Link
+          href="https://nextjs.org/"
+          target="_blank"
+          className="font-bold hover:underline"
+          rel="noreferrer"
+        >
+        Next.js
         </Link>
+        {", "}
+        <Link
+          href="https://tailwindcss.com/"
+          target="_blank"
+          className="font-bold hover:underline"
+          rel="noreferrer"
+        >
+        Tailwind CSS
+        </Link>{", and "}
+        <Link
+          href="https://clerk.com/"
+          target="_blank"
+          className="font-bold hover:underline"
+          rel="noreferrer"
+          >
+          Clerk
+        </Link>.
       </p>
     </footer>
   );
