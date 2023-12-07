4cd8d48e22b6ef5c153d88e58aa09b5b9738a833
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-12-07 21:32:38 +1300

    contentlayer blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,27 +1,65 @@
+import Image from "next/image";
 import Link from "next/link";
-import dayjs from "dayjs";
+import { allPosts } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
 
-import { api } from "~/trpc/server";
+export const metadata = {
+  title: "Blog",
+};
 
-export const revalidate = 3600;
-
-export default async function Page() {
-  const posts = await api.post.getAll.query();
+export default async function BlogPage() {
+  const posts = allPosts
+    .filter((post) => post.published)
+    .sort((a, b) => {
+      return compareDesc(new Date(a.date), new Date(b.date));
+    });
 
   return (
-    <div className="min-h-screen items-center">
-      <section className="text-center">
-        <h2 className="my-4 text-6xl font-bold">Blog</h2>
-      </section>
-      <ul className="my-auto leading-loose text-foreground">
-        {posts?.map((post) => (
-          <Link key={post.id} href={`/blog/${post.id}`}>
-            <li className="underline">
-              {post.title} - {dayjs(post.created_at).format("DD MMM YYYY")}
-            </li>
-          </Link>
-        ))}
-      </ul>
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-y-4">
+          <h1 className="font-heading inline-block text-4xl tracking-tight lg:text-5xl">
+            Blog
+          </h1>
+          <p className="text-xl text-muted-foreground">
+            A blog built using Contentlayer. Posts are written in MDX.
+          </p>
+        </div>
+      </div>
+      <hr className="my-8" />
+      {posts?.length ? (
+        <div className="grid gap-10 sm:grid-cols-2">
+          {posts.map((post, index) => (
+            <article
+              key={post._id}
+              className="group relative flex flex-col space-y-2"
+            >
+              {post.image && (
+                <Image
+                  src={post.image}
+                  alt={post.title}
+                  width={804}
+                  height={452}
+                  className="rounded-md border bg-muted transition-colors"
+                  priority={index <= 1}
+                />
+              )}
+              <h2 className="text-2xl font-extrabold">{post.title}</h2>
+              {post.description && (
+                <p className="text-muted-foreground">{post.description}</p>
+              )}
+              {post.date && (
+                <p className="text-sm text-muted-foreground">{post.date}</p>
+              )}
+              <Link href={post.slug} className="absolute inset-0">
+                <span className="sr-only">View Article</span>
+              </Link>
+            </article>
+          ))}
+        </div>
+      ) : (
+        <p>No posts published.</p>
+      )}
     </div>
   );
 }
