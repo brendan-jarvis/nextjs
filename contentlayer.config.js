7c5a8e0a1cb5107e93398a22c34e06f38872fefa
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-12-09 17:32:40 +1300

    Project doc type

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -53,9 +53,50 @@ export const Post = defineDocumentType(() => ({
   computedFields,
 }));
 
+export const Project = defineDocumentType(() => ({
+  name: "Project",
+  filePathPattern: `projects/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+    },
+    date: {
+      type: "date",
+      required: true,
+    },
+    published: {
+      type: "boolean",
+      default: true,
+    },
+    image: {
+      type: "string",
+      required: false,
+    },
+    url: {
+      type: "string",
+      required: true,
+    },
+    authors: {
+      // Reference types are not embedded.
+      // Until this is fixed, we can use a simple list.
+      // type: "reference",
+      // of: Author,
+      type: "list",
+      of: { type: "string" },
+      required: true,
+    },
+  },
+  computedFields,
+}));
+
 export default makeSource({
   contentDirPath: "./content",
-  documentTypes: [Post],
+  documentTypes: [Post, Project],
   mdx: {
     //remarkPlugins: [remarkGfm],
     rehypePlugins: [
